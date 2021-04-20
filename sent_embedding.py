c76745c9489fa531ff28fd523149ad78b7af06b0
psteam <psteam@safp.gov.mo>
2021-04-20 15:12:31 +0800

    update

--- sent_embedding.py
+++ sent_embedding.py
@@ -1,24 +1,56 @@
 from sentence_transformers import SentenceTransformer
 import jieba
+import jieba.analyse
+import pandas as pd
+import re
+from sklearn.cluster import KMeans
 
 model = SentenceTransformer('distiluse-base-multilingual-cased-v1')
 
-# Our sentences we like to encode
-sentences = ['人生自古誰無死',
-             '每個人一天內都需要做兩件他討厭的事',
-             '你站在歷史的正確面']
+headers = ["department", "lei", "chapter", "chtitle", "article", "arttitle", "content"]
+data = pd.read_csv("lei_cleaned.csv", names=headers)
 
-# Sentences are encoded by calling model.encode()
-sentence_embeddings = model.encode(sentences)
+jieba.set_dictionary('./dict.txt')
+
+stopwords = set([])
+
+with open('stop_words.txt') as f:
+    words = f.readlines()
+
+for word in words:
+    stopwords.add(word.strip('\n'))
 
 corpus = []
 
-for s in sentences:
-    k = jieba.cut(s)
-    corpus.append(' '.join(k))
+for item in data['content']:
+    words = jieba.cut(str(item))
+    words = [w for w in words if w not in stopwords]
+    words = [w for w in words if len(w) > 1 and not re.match('^[a-z|A-Z|0-9|.]*$', w)]
+    item = ' '.join(words)
+    corpus.append(item)
 
-# Print the embeddings
-for sentence, embedding in zip(corpus, sentence_embeddings):
-    print("Sentence:", sentence)
-    print("Embedding:", embedding)
+# Sentences are encoded by calling model.encode()
+corpus_embeddings = model.encode(corpus)
+
+# corpus = []
+#
+# for s in sentences:
+#     k = jieba.cut(s)
+#     corpus.append(' '.join(k))
+
+# Perform kmean clustering
+num_clusters = 10
+clustering_model = KMeans(n_clusters=num_clusters)
+clustering_model.fit(corpus_embeddings)
+cluster_assignment = clustering_model.labels_
+
+clustered_sentences = [[] for i in range(num_clusters)]
+for sentence_id, cluster_id in enumerate(cluster_assignment):
+    clustered_sentences[cluster_id].append(corpus[sentence_id])
+
+for i, cluster in enumerate(clustered_sentences):
+    print("Cluster ", i+1)
+    print(cluster)
+    with open('cluster_{}'.format(i), 'w') as f:
+        f.writelines(["%s\n" % item for item in cluster])
     print("")
